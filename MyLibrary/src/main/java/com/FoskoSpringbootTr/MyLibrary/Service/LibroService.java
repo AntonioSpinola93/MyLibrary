3c0b67fe685e9bc81ae1a383e761c2f19f3ab42c
Antonio Spinola <onibi93@gmail.com>
2023-10-09 13:11:18 +0200

    Creato metodo add e findALl di LibroService

--- MyLibrary/src/main/java/com/FoskoSpringbootTr/MyLibrary/Service/LibroService.java
+++ MyLibrary/src/main/java/com/FoskoSpringbootTr/MyLibrary/Service/LibroService.java
@@ -1,8 +1,13 @@
 package com.FoskoSpringbootTr.MyLibrary.Service;
 
+import com.FoskoSpringbootTr.MyLibrary.Dto.LibroDto;
+import com.FoskoSpringbootTr.MyLibrary.Entity.Libro;
 import com.FoskoSpringbootTr.MyLibrary.Repository.LibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 
@@ -12,7 +17,26 @@ public class LibroService {
     public LibroService(LibroRepository libroRepository){
         this.libroRepository=libroRepository;
     }
-
-
-
+    public LibroDto addLibroDto(LibroDto libroDto){
+        Libro libro=new Libro();
+        libro.setNomeProdotto(libroDto.getNomeProdotto());
+        libro.setAutore(libroDto.getAutore());
+        libro.setIsbn(libroDto.getIsbn());
+        libro.setPrezzo(libroDto.getPrezzo());
+        libroRepository.save(libro);
+        libroDto.setNomeProdotto(libro.getNomeProdotto());
+        libroDto.setAutore(libro.getAutore());
+        libroDto.setIsbn(libro.getIsbn());
+        libroDto.setPrezzo(libro.getPrezzo());
+        return libroDto;
+    }
+    public List<LibroDto> findAll(){
+        List<Libro> listaLibri= libroRepository.findAll();
+        List<LibroDto>libroDtoList=new ArrayList<>();
+        for(Libro libro:listaLibri){
+            libroDtoList.add( new LibroDto(libro.getId(),libro.getNomeProdotto(),libro.getAutore(),
+                    libro.getIsbn(),libro.getPrezzo()));
+        }
+        return libroDtoList;
+    }
 }
